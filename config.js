92e515e23f96a0c24edcece7073437b85128c98b
m-triple-m <triplem656@gmail.com>
2021-06-18 10:47:25 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const api_config = {
+    port: 5000 || process.env.PORT,
+    db_url: 'mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/reactDB?retryWrites=true&w=majority'
+}
+
+module.exports = api_config;
